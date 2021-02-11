9c80481d18c505fcff72e265536d460a07247ffc
DELAmin <delamin13@gmail.com>
2021-02-11 23:44:14 +0100

    Migrations - makmigrstions - the lecture 4

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'flights',
-    'django.contrib.tadmin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
